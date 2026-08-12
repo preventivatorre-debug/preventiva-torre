04bee9f7b144c1bbc9cb0ef94a42effb86bd4ed7
preventivatorre-debug <preventivatorre@gmail.com>
2026-08-12 17:14:58 -0300

    Create service-worker.js

---
+++ autonomia/service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "autonomia-pops-v1";
+
+const ARQUIVOS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ARQUIVOS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copia = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copia);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
