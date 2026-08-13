1707ee7a51b8f0447166520f56cf118bc12aa405
preventivatorre-debug <preventivatorre@gmail.com>
2026-08-13 14:51:50 -0300

    Update service-worker.js

--- autonomia/service-worker.js
+++ autonomia/service-worker.js
@@ -1,76 +1,19 @@
-const CACHE_NAME = "autonomia-pops-v3";
-
-const ARQUIVOS = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-// INSTALAÇÃO
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ARQUIVOS))
-      .then(() => self.skipWaiting())
-  );
+    self.skipWaiting();
 });
 
-// ATIVAÇÃO
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    }).then(() => self.clients.claim())
-  );
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => caches.delete(key))
+            )
+        ).then(() => self.clients.claim())
+    );
 });
 
-// REQUISIÇÕES
 self.addEventListener("fetch", event => {
-
-  // Só trata requisições GET
-  if (event.request.method !== "GET") {
-    return;
-  }
-
-  event.respondWith(
-
-    fetch(event.request)
-      .then(response => {
-
-        // Não salva respostas inválidas
-        if (!response || response.status !== 200) {
-          return response;
-        }
-
-        const copia = response.clone();
-
-        caches.open(CACHE_NAME)
-          .then(cache => {
-            cache.put(event.request, copia);
-          })
-          .catch(() => {});
-
-        return response;
-      })
-
-      .catch(() => {
-
-        return caches.match(event.request)
-          .then(cached => {
-
-            if (cached) {
-              return cached;
-            }
-
-            return caches.match("./index.html");
-          });
-
-      })
-
-  );
-
+    event.respondWith(
+        fetch(event.request)
+    );
 });
